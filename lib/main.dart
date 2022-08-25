160ed3e986c07ef7286de47d00f28b7b12823a83
unknown <ihernanhergert@gmail.com>
2022-08-24 23:01:17 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:chatapp/common/utils/colors.dart';
+import 'package:chatapp/common/widgets/error.dart';
+import 'package:chatapp/common/widgets/loader.dart';
+import 'package:chatapp/features/auth/controller/auth_controller.dart';
+import 'package:chatapp/features/landing/screens/landing_screen.dart';
+import 'package:chatapp/router.dart';
+import 'package:chatapp/mobile_layout_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends ConsumerWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Chatapp',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+        appBarTheme: const AppBarTheme(
+          color: appBarColor,
+        ),
+      ),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: ref.watch(userDataAuthProvider).when(
+            data: (user) {
+              if (user == null) {
+                return const LandingScreen();
+              }
+              return const MobileLayoutScreen();
+            },
+            error: (err, trace) {
+              return ErrorScreen(
+                error: err.toString(),
+              );
+            },
+            loading: () => const Loader(),
+          ),
+    );
+  }
+}
